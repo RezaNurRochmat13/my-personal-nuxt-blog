4e7244182e45a1e3a5ab4c53562f88c48537bfab
Reja <rezanurrochmat@gmail.com>
2023-09-05 14:39:20 +0700

    add accordion and update header

--- pages/home.vue
+++ pages/home.vue
@@ -2,12 +2,14 @@
   <div class="container">
     <Jumbotron />
     <Gallery />
+    <Faq />
   </div>
 </template>
 
 <script setup lang="ts">
 import Jumbotron from "../components/Jumbotron.vue";
 import Gallery from "~/components/Gallery.vue";
+import Faq from "~/components/Faq.vue";
 </script>
 
 <style scoped></style>
