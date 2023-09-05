4823d5c9cf3fbabba709c4864bde50e9f975d6b9
Reja <rezanurrochmat@gmail.com>
2023-09-05 14:09:32 +0700

    add jumbotron and photo gallery

---
+++ pages/home.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="container">
+    <Jumbotron />
+    <Gallery />
+  </div>
+</template>
+
+<script setup lang="ts">
+import Jumbotron from "../components/Jumbotron.vue";
+import Gallery from "~/components/Gallery.vue";
+</script>
+
+<style scoped></style>
